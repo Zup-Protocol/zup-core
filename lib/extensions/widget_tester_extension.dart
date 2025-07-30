7c9bde523f7fc3d7d15d2fddcd2161faf1589e8d
Ryan Holanda <ryanholanda08@gmail.com>
2025-07-30 17:39:08 -0300

    fix: unhover doesnt work in widget tester extension

--- lib/extensions/widget_tester_extension.dart
+++ lib/extensions/widget_tester_extension.dart
@@ -6,22 +6,22 @@ extension WidgetTesterExtension on WidgetTester {
   /// Simulates a mouse hovering the passed [find] (or widget) on the screen.
   ///
   /// Useful for testing hover states
-  Future<void> hover(Finder find) async {
+  Future<TestGesture> hover(Finder find) async {
     final gesture = await createGesture(kind: PointerDeviceKind.mouse);
     await gesture.addPointer(location: Offset.zero);
 
     await gesture.moveTo(getCenter(find));
+
+    return gesture;
   }
 
-  /// Removes the hover state from the passed [find] (or widget) on the screen.
+  /// Removes the hover state from a passed hover gesture
   ///
   /// Useful for removing the hover when testing multiple widgets or states of one widget
   ///
-  /// Note that first you should call [hover] to after [unHover] it
-  Future<void> unHover(Finder find) async {
-    final gesture = await createGesture(kind: PointerDeviceKind.mouse);
-    await gesture.addPointer(location: Offset.zero);
-
-    gesture.cancel();
+  /// Note that first you should call [hover] to after [unHover] it.
+  Future<void> unHover(TestGesture hoverGesture) async {
+    await hoverGesture.down(Offset.zero);
+    await hoverGesture.cancel();
   }
 }
