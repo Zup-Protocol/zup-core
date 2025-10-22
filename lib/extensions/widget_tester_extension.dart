554a229c2b19c658d04586e9d6bd99979ff12d28
Ryan Holanda <ryanholanda08@gmail.com>
2025-10-22 20:15:46 -0300

    feat: add widget tester extensions to move hover to another places

--- lib/extensions/widget_tester_extension.dart
+++ lib/extensions/widget_tester_extension.dart
@@ -15,6 +15,32 @@ extension WidgetTesterExtension on WidgetTester {
     return gesture;
   }
 
+  /// Simulates a mouse moving from one point, to another, like if a mouse
+  /// was hovering something and then move to another place.
+  ///
+  /// [to]: Offset to move the hover by
+  ///
+  /// [hoverGesture]: The hover gesture used to hover something before.
+  /// In case you don't have it, using the function [hover] will return
+  /// it by default.
+  Future<void> moveHoverByOffset(Offset to, TestGesture hoverGesture) async {
+    await hoverGesture.moveBy(to);
+  }
+
+  /// Simulates a mouse moving from one widget to another.
+  ///
+  /// [find] the finder object of the new widget to move the mouse to.
+  ///
+  /// [hoverGesture]: The hover gesture used to hover something before.
+  /// In case you don't have it, using the function [hover] will return
+  /// it by default.
+  ///
+  /// This function is useful when you want to test the hover state of a widget
+  /// after the user hover another widget
+  Future<void> moveHoverToWidget(Finder find, TestGesture hoverGesture) async {
+    await hoverGesture.moveTo(getCenter(find));
+  }
+
   /// Removes the hover state from a passed hover gesture
   ///
   /// Useful for removing the hover when testing multiple widgets or states of one widget
